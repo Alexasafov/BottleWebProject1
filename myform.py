c12cb5f2ebff12cbdf3aa163ba3c1b56552b6196
Alexander <azazf93@gmail.com>
2024-04-16 17:50:51 +0300

    Add debug and question dictionary

--- myform.py
+++ myform.py
@@ -1,8 +1,13 @@
 from bottle import post, request
 import re
 from datetime import datetime
+import pdb
+import json
+import os
 
-regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+REGEX = r'\b[A-Za-z0-9._%+-]+@+[A-Z|a-z]+[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+questions = {}
 
 @post('/home', method='post')
 def my_form():
@@ -10,10 +15,14 @@ def my_form():
     quest = request.forms.get('QUEST')
     username = request.forms.get('USERNAME')
     date_time = datetime.now().strftime("%Y-%m-%d")
+    
     if quest == "" or mail == "" or username == "":
         return "Not all fields filled"
-    elif not re.fullmatch(regex, mail):
+    if not re.fullmatch(REGEX, mail):
         return "Invalid mail"
-    else:
-        return f'''Thanks, {username}! The answer will be sent to the mail {mail}
+    questions[mail] = quest
+    pdb.set_trace()
+
+    return f'''Thanks, {username}! The answer will be sent to the mail {mail}
             Access Date: {date_time}'''
+        
