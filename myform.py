7a36aff41be29a33176c5544d7182ebd97ac6430
Alexander <azazf93@gmail.com>
2024-04-16 18:43:28 +0300

    Add saving data about questions to a json file and validating questions

--- myform.py
+++ myform.py
@@ -1,11 +1,16 @@
 from bottle import post, request
 import re
 from datetime import datetime
-import pdb
 import json
 import os
 
 REGEX = r'\b[A-Za-z0-9._%+-]+@+[A-Z|a-z]+[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+WRITEPATH = 'questions.json'
+
+def add_if_not_exist(list, el):
+     if el not in list:
+        return list + [el]
+     return list
 
 questions = {}
 
@@ -15,13 +20,27 @@ def my_form():
     quest = request.forms.get('QUEST')
     username = request.forms.get('USERNAME')
     date_time = datetime.now().strftime("%Y-%m-%d")
-    
+
     if quest == "" or mail == "" or username == "":
         return "Not all fields filled"
     if not re.fullmatch(REGEX, mail):
         return "Invalid mail"
-    questions[mail] = [username, quest]
-    pdb.set_trace()
+    # длина вопроса не может быть меньше 4 символов
+    # вопрос не может состоять только из цифр
+    if len(quest) <= 3 or quest.isdigit():
+        return "Error: Invalid question."
+    
+    # создаем файл если его нет
+    if not os.path.exists(WRITEPATH):
+        with open(WRITEPATH, 'a+') as json_file:
+            json.dump({}, json_file) 
+    with open(WRITEPATH, 'r+') as json_file:
+        questions = json.load(json_file)
+        # добавляем вопрос если его нет в списке
+        questions[mail] = add_if_not_exist(questions.get(mail, []), quest)
+        # перезаписываем файл с новыми файлами
+        json_file.seek(0)
+        json.dump(questions, json_file)
 
     return f'''Thanks, {username}! The answer will be sent to the mail {mail}
             Access Date: {date_time}'''
